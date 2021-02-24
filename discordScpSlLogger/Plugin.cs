d493558ba45cf43081f6323e9ed808a1836ab63c
Evitonative <47825249+Evitonative@users.noreply.github.com>
2021-02-24 18:02:30 +0100

    ToDos for Whats to come [Only player and Server]

--- discordScpSlLogger/Plugin.cs
+++ discordScpSlLogger/Plugin.cs
@@ -25,25 +25,115 @@ namespace discordScpSlLogger
         {
             server = new Handlers.Server();
             player = new Handlers.Player();
-
+            
+            //Server Events
+            //Commands
             Server.SendingRemoteAdminCommand += server.SendingRemoteAdminCommand;
+            //TODO: Server.SendingConsoleCommand += server.SendingConsoleCommand;
+            //Round Events
             Server.WaitingForPlayers += server.WaitingForPlayers;
+            //TODO: Server.RoundStarted += server.RoundStarted;
+            //TODO: Server.EndingRound += server.RoundEnded;
+            //TODO: Server.RespawningTeam += server.RespawningTeam;
+            //Reporting
+            //TODO: Server.LocalReporting += server.LocalReporting;
+            //TODO: Server.ReportingCheater += server.ReportingCheater;
 
+            //Player Connection and Punishments
             Player.Verified += player.Joined;
             Player.Destroying += player.Left;
             Player.Kicked += player.Kicked;
             Player.Banned += player.Banned;
+            //TODO: Player.ChangingIntercomMuteStatus += player.ChangingIntercomMuteStatus;
+            //TODO: Player.ChangingMuteStatus += player.ChangingMuteStatus;
+
+            //General Stuff
+            //TODO: Player.ActivatingWarheadPanel += player.ActivatingWarheadPanel;
+            //TODO: Player.ActivatingWorkstation += player.ActivatingWorkstation;
+            //TODO: Player.DeactivatingWorkstation += player.DeactivatingWorkstation;
+            //TODO: Player.ChangingItem += player.ChangingItem;
+            //TODO: .DroppingItem += player.DroppingItem;
+            //TODO: Player.UsingMedicalItem += player.UsingMedicalItem;
+            //TODO: Player.StoppingMedicalItem += player.StoppingMedicalItem;
+            //TODO: Player.ReloadingWeapon += player.ReloadingWeapon;
+            //TODO: Player.ChangingRole += player.ChangingRole;
+            //TODO: Player.Died += player.Died;
+            //TODO: Player.UnlockingGenerator += player.UnlockingGenerator;
+            //TODO: Player.OpeningGenerator += player.OpeningGenerator;
+            //TODO: Player.InsertingGeneratorTablet += player.InsertingGeneratorTablet;
+            //TODO: Player.EjectingGeneratorTablet += player.EjectingGeneratorTablet;
+            //TODO: Player.ClosingGenerator += player.ClosingGenerator;
+            //TODO: Player.EjectingGeneratorTablet += player.EjectingGeneratorTablet;
+            //TODO: Player.IntercomSpeaking += player.IntercomSpeaking;
+
+            //Handcuffs
+            //TODO: Player.Handcuffing += player.Handcuffing;
+            //TODO: Player.RemovingHandcuffs += player.RemovingHandcuffs;
+
+            //Interactions
+            //TODO: Player.InteractingDoor += player.InteractingDoor;
+            //TODO: Player.InteractingElevator += player.InteractingElevator;
+            //TODO: Player.InteractingLocker += player.InteractingLocker;
+
+            //106 Stuff
+            //TODO: Player.EnteringFemurBreaker += player.EnteringFemurBreaker;
+            //TODO: Player.EnteringPocketDimension += player.EnteringPocketDimension;
         }
 
         public override void OnDisabled()
         {
+            //Server Events
+            //Commands
             Server.SendingRemoteAdminCommand -= server.SendingRemoteAdminCommand;
+            //TODO: Server.SendingConsoleCommand -= server.SendingConsoleCommand;
+            //Round Events
             Server.WaitingForPlayers -= server.WaitingForPlayers;
-            
+            //TODO: Server.RoundStarted -= server.RoundStarted;
+            //TODO: Server.EndingRound -= server.RoundEnded;
+            //TODO: Server.RespawningTeam -= server.RespawningTeam;
+            //Reporting
+            //TODO: Server.LocalReporting -= server.LocalReporting;
+            //TODO: Server.ReportingCheater -= server.ReportingCheater;
+
+            //Player Connection and Punishments
             Player.Verified -= player.Joined;
             Player.Destroying -= player.Left;
             Player.Kicked -= player.Kicked;
             Player.Banned -= player.Banned;
+            //TODO: Player.ChangingIntercomMuteStatus -= player.ChangingIntercomMuteStatus;
+            //TODO: Player.ChangingMuteStatus -= player.ChangingMuteStatus;
+
+            //General Stuff
+            //TODO: Player.ActivatingWarheadPanel -= player.ActivatingWarheadPanel;
+            //TODO: Player.ActivatingWorkstation -= player.ActivatingWorkstation;
+            //TODO: Player.DeactivatingWorkstation -= player.DeactivatingWorkstation;
+            //TODO: Player.ChangingItem -= player.ChangingItem;
+            //TODO: .DroppingItem -= player.DroppingItem;
+            //TODO: Player.UsingMedicalItem -= player.UsingMedicalItem;
+            //TODO: Player.StoppingMedicalItem -= player.StoppingMedicalItem;
+            //TODO: Player.ReloadingWeapon -= player.ReloadingWeapon;
+            //TODO: Player.ChangingRole -= player.ChangingRole;
+            //TODO: Player.Died -= player.Died;
+            //TODO: Player.UnlockingGenerator -= player.UnlockingGenerator;
+            //TODO: Player.OpeningGenerator -= player.OpeningGenerator;
+            //TODO: Player.InsertingGeneratorTablet -= player.InsertingGeneratorTablet;
+            //TODO: Player.EjectingGeneratorTablet -= player.EjectingGeneratorTablet;
+            //TODO: Player.ClosingGenerator -= player.ClosingGenerator;
+            //TODO: Player.EjectingGeneratorTablet -= player.EjectingGeneratorTablet;
+            //TODO: Player.IntercomSpeaking -= player.IntercomSpeaking;
+
+            //Handcuffs
+            //TODO: Player.Handcuffing -= player.Handcuffing;
+            //TODO: Player.RemovingHandcuffs -= player.RemovingHandcuffs;
+
+            //Interactions
+            //TODO: Player.InteractingDoor -= player.InteractingDoor;
+            //TODO: Player.InteractingElevator -= player.InteractingElevator;
+            //TODO: Player.InteractingLocker -= player.InteractingLocker;
+
+            //106 Stuff
+            //TODO: Player.EnteringFemurBreaker -= player.EnteringFemurBreaker;
+            //TODO: Player.EnteringPocketDimension -= player.EnteringPocketDimension;
 
             server = null;
         }
