e7929265ddef2beea13b129033269dd29ca66771
Evitonative <47825249+Evitonative@users.noreply.github.com>
2021-03-07 12:21:06 +0100

    Moved code Blocks
    This is so the spoilers from the ips still works.

--- discordScpSlLogger/Config.cs
+++ discordScpSlLogger/Config.cs
@@ -23,12 +23,12 @@ namespace discordScpSlLogger
         public string WaitingForPlayers { get; set; } = ":beginner: Waiting for players...";
 
         [Description("Join, leave, kick, ban")]
-        public string PlayerJoined { get; set; } = ":arrow_forward: `$Name($SteamId | $Id | ||$Ip||)` joined.";
-        public string PlayerLeft { get; set; } = ":arrow_backward: `$Name($SteamId | $Id | ||$Ip||)` left.";
+        public string PlayerJoined { get; set; } = ":arrow_forward: `$Name`($SteamId | $Id | ||$Ip||) joined.";
+        public string PlayerLeft { get; set; } = ":arrow_backward: `$Name`($SteamId | $Id | ||$Ip||) left.";
         public bool PlayerKickedInRa { get; set; } = false;
-        public string PlayerKicked { get; set; } = ":no_entry: `$Name($SteamId | $Id | ||$Ip||)` has been kicked for `$Reason`.";
+        public string PlayerKicked { get; set; } = ":no_entry: `$Name`($SteamId | $Id | ||$Ip||) has been kicked for `$Reason`.";
         public bool PlayerBannedInRa { get; set; } = false;
-        public string PlayerBanned { get; set; } = ":name_badge: `$Name($SteamId | $Id | ||$Ip||)` has been banned by `$IssuerName ($IssuerSteamId | $IssuerId | ||$IssuerIp||)` for `$Reason` for $BanTime";
+        public string PlayerBanned { get; set; } = ":name_badge: `$Name`($SteamId | $Id | ||$Ip||) has been banned by `$IssuerName`($IssuerSteamId | $IssuerId | ||$IssuerIp||) for `$Reason` for $BanTime";
         public string BanTimeFormat { get; set; } = "$dayd $hrsh $minm";
     }
 }
