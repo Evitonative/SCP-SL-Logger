773e0e8732ffd1c02d3a6dacda2e74d729b3994f
Evitonative <47825249+Evitonative@users.noreply.github.com>
2021-03-07 11:58:52 +0100

    Fixed the posibility that someone names @everyone @here  <@123> <@&123>

--- discordScpSlLogger/Config.cs
+++ discordScpSlLogger/Config.cs
@@ -13,22 +13,22 @@ namespace discordScpSlLogger
 
         [Description("RaLog")]
         public bool RaLogEnabled { get; set; } = true;
-        public string RaLog { get; set; } = ":notepad_spiral: $user($steamid) used RA command: $cmd $args | Permitted: $allowed";
+        public string RaLog { get; set; } = ":notepad_spiral: `$user($steamid)` used RA command: `$cmd $args` | Permitted: $allowed";
         
         [Description("Console Log")]
         public bool ConsoleLogInRaLog { get; set; } = true;
-        public string ConsoleLog { get; set; } = ":notepad_spiral: $user($steamid) used Normal command: $cmd $args | Permitted: $allowed";
+        public string ConsoleLog { get; set; } = ":notepad_spiral: `$user($steamid)` used Normal command: `$cmd $args` | Permitted: $allowed";
 
         [Description("Waiting for Players")]
         public string WaitingForPlayers { get; set; } = ":beginner: Waiting for players...";
 
         [Description("Join, leave, kick, ban")]
-        public string PlayerJoined { get; set; } = ":arrow_forward: $Name($SteamId | $Id | ||$Ip||) joined.";
-        public string PlayerLeft { get; set; } = ":arrow_backward: $Name($SteamId | $Id | ||$Ip||) left.";
+        public string PlayerJoined { get; set; } = ":arrow_forward: `$Name($SteamId | $Id | ||$Ip||)` joined.";
+        public string PlayerLeft { get; set; } = ":arrow_backward: `$Name($SteamId | $Id | ||$Ip||)` left.";
         public bool PlayerKickedInRa { get; set; } = false;
-        public string PlayerKicked { get; set; } = ":no_entry: $Name($SteamId | $Id | ||$Ip||) has been kicked for `$Reason`.";
+        public string PlayerKicked { get; set; } = ":no_entry: `$Name($SteamId | $Id | ||$Ip||)` has been kicked for `$Reason`.";
         public bool PlayerBannedInRa { get; set; } = false;
-        public string PlayerBanned { get; set; } = ":name_badge: $Name($SteamId | $Id | ||$Ip||) has been banned by $IssuerName($IssuerSteamId | $IssuerId | ||$IssuerIp||) for $Reason for $BanTime";
+        public string PlayerBanned { get; set; } = ":name_badge: `$Name($SteamId | $Id | ||$Ip||)` has been banned by `$IssuerName  ($IssuerSteamId | $IssuerId | ||$IssuerIp||)` for `$Reason` for $BanTime";
         public string BanTimeFormat { get; set; } = "$dayd $hrsh $minm";
     }
 }
